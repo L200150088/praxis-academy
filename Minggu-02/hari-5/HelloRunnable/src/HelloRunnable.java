b7f0b217573a454afea131f5a24b913bc4f60360
khofa prayogo <khofaprayogo26@gmail.com>
2019-06-28 14:25:11 +0700

    hari-5

---
+++ Minggu-02/hari-5/HelloRunnable/src/HelloRunnable.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class HelloRunnable{
+     public static void main(String args[])
+        throws InterruptedException {
+        String importantInfo[] = {
+            "Mares eat oats",
+            "Does eat oats",
+            "Little lambs eat ivy",
+            "A kid will eat ivy too"
+        };
+
+       for (int i = 0; i < importantInfo.length; i++) {
+    // Pause for 4 seconds
+    try {
+        Thread.sleep(4000);
+    } catch (InterruptedException e) {
+        // We've been interrupted: no more messages.
+        return;
+    }
+    // Print a message
+    System.out.println(importantInfo[i]);
+}
+     }
+
+    public void run() {
+        throw new UnsupportedOperationException("Gagal..."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
