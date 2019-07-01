52a6bb16282e43f096ef69127db12985241c9419
khofa prayogo <khofaprayogo26@gmail.com>
2019-07-01 14:20:21 +0700

    Update..

---
+++ Minggu-03/Latihan_Java_Dependency_Injection/src/main/java/com/journaldev/java/dependencyinjection/consumer/MyDIApplication.java
@@ -0,0 +1,20 @@
+package com.journaldev.java.dependencyinjection.consumer;
+
+import com.journaldev.java.dependencyinjection.service.MessageService;
+
+public class MyDIApplication implements Consumer{
+
+	private MessageService service;
+	public MyDIApplication(){}
+	
+	public MyDIApplication(MessageService service){
+		this.service=service;
+	}
+	
+	@Override
+	public void processMessages(String msg, String rec){
+		//do some msg validation, manipulation logic etc
+		this.service.sendMessage(msg, rec);
+	}
+
+}
