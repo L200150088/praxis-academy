1e9bcea9a0b2d85bba5b34ebe3457011be0f73c9
khofa prayogo <khofaprayogo26@gmail.com>
2019-07-01 11:52:27 +0700

    Update...

---
+++ Minggu-03/Latihan6-minggu3-hari1/src/main/java/Latihan6/minggu3/hari1/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package Latihan6.minggu3.hari1;
+
+public interface AdvancedMediaPlayer {
+	 public void playVlc(String fileName);
+	   public void playMp4(String fileName);
+}
---
+++ Minggu-03/Latihan6-minggu3-hari1/src/main/java/Latihan6/minggu3/hari1/MediaPlayer.java
@@ -0,0 +1,5 @@
+package Latihan6.minggu3.hari1;
+
+public interface MediaPlayer {
+	public void play(String audioType, String fileName);
+}
